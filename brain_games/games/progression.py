94c9b0c15c427a2e996bb2fd384001c30e33abab
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-01-17 20:13:34 +0300

    corrected based on teacher comments 4

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,26 +3,19 @@ import random
 from brain_games.consts import INSTRUCTION_PROGRESSION
 
 
-def get_progression():
-    num = random.randint(1, 100)
-    number_of_elements = random.randint(5, 10)
-    step = random.randint(1, 10)
-    counter = 1
-
-    index = random.randint(0, number_of_elements - 1)
-    numbers = (num, )
-    progression = ' ' + str(num) + ' '
-    while counter < number_of_elements:
-        num += step
-        progression += str(num) + ' '
-        numbers += (num, )
-        counter += 1
-
-    number_to_hide = ' ' + str(numbers[index]) + ' '
-    question = progression.replace(number_to_hide, ' .. ').strip()
-    result = number_to_hide.strip()
-    return question, str(result)
+def get_progression_and_ans():
+    num, step = random.randint(1, 100), random.randint(1, 10)
+    progr = [num]
+    for i in range(random.randint(5, 10)):
+        progr.append(progr[-1] + step)
+    right_answer = ' ' + str(progr[random.randint(0, len(progr) - 1)]) + ' '
+    progr_str = str(progr)
+    progr_str = ' ' + progr_str + ' '
+    progr_str = progr_str.replace(',', '').replace('[', '').replace(']', '')
+    progr_str = progr_str.replace(right_answer, ' .. ')
+    right_answer = right_answer.strip()
+    return progr_str, right_answer
 
 
 def run_progression_game():
-    run_game(get_progression, INSTRUCTION_PROGRESSION)
+    run_game(get_progression_and_ans, INSTRUCTION_PROGRESSION)
